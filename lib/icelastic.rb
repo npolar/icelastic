217884444692db4f9665b3574531e4fbb9bbe7ce
RDux <ruben.dens@gmail.com>
2013-11-12 09:50:18 +0100

    added requirements

--- lib/icelastic.rb
+++ lib/icelastic.rb
@@ -1,7 +1,13 @@
+require "time"
+require "json"
+require "rack"
+require "elasticsearch"
+
 require "icelastic/version"
 require "icelastic/query"
 require "icelastic/result"
 require "icelastic/client"
+require "rack/icelastic"
 
 module Icelastic
 end
