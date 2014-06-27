bf34e5790d2e01095ff37934ce40fe2a72569cf8
RDuX <ruben.dens@gmail.com>
2014-06-27 11:32:49 +0200

    start a test cluster at elasticsearch runtime and seed the test index. at exit kill the cluster

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,7 +9,7 @@ SimpleCov.start do
   add_filter do |src|
     src.lines.count < 5
   end
-  
+
   # Groups
   add_group "Lib", "/lib/icelastic"
   add_group "Middleware", "/lib/rack"
@@ -19,7 +19,22 @@ require "bundler/setup"
 require 'rspec'
 require 'rack/test'
 require 'icelastic'
+require 'elasticsearch/extensions/test/cluster'
 
 RSpec.configure do |conf|
   conf.include( Rack::Test::Methods )
 end
+
+## Elasticsearch Server Testing
+
+# start a test cluster
+Elasticsearch::Extensions::Test::Cluster.start \
+  cluster_name: "icespec",
+  command:      ENV['TEST_CLUSTER_COMMAND'] ||= "/tmp/elasticsearch/bin/elasticsearch",
+  port:         9350,
+  nodes:        1
+
+# create a test index in the test cluster
+Elasticsearch::Client.new( url: "http://localhost:9350" ).index index: 'rspec', type: 'spec', id: 1, body: { title: 'IceSpec' }
+
+at_exit{ Elasticsearch::Extensions::Test::Cluster.stop port: 9350 }
