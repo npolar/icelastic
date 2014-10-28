ca1e2c68e6a6cd582cbde8853da4f22d135b50f7
cnrdh <cnrdh@users.noreply.github.com>
2014-10-28 13:17:52 +0100

    Using media type from injected writer

--- lib/rack/icelastic.rb
+++ lib/rack/icelastic.rb
@@ -11,26 +11,50 @@ module Rack
     attr_accessor :env, :params
 
     def initialize(app=nil, options={})
-      app = lambda{|env| [200, {"Content-Type" => "application/json"}, [{"error" => "404 Not Found"}.to_json]]} if app.nil?
-
+      if app.nil?
+        app = lambda{|env| [200, {"Content-Type" => "application/json"}, [{"error" => "404 Not Found"}.to_json]]}
+      end
+      if not options.key? :writers
+        options[:writers] = ::Icelastic::Default.writers
+      end
+      
       @app, @config = app, options
     end
-
+    
     def call(env)
-      self.env = env
-      self.params = CGI.parse(env['QUERY_STRING'])
-
-      headers = csv? ? ({"Content-Type" => "text/plain; charset=utf-8"}) : ({"Content-Type" => "application/json; charset=utf-8"})
-      # Execute a search and return the response if a search request
-      return [200, headers, [client.search(request)]] if search?
-
-      @app.call(env)
+      
+      @env = env
+      @params = CGI.parse(env['QUERY_STRING'])
+      
+      if not search?
+        return @app.call(env)
+      end
+      
+      body = client.search(request)
+      # @todo How do you get the real HTTP status from elasticsearch-ruby?
+      [200, {"Content-Type" => "#{type(format)}; charset=utf-8"}, [body]] 
     end
 
-    private
+    protected
 
     def request
       Rack::Request.new(env)
+    end
+    
+    def type(format)
+      if format == "raw"
+        return "application/json"
+      end
+      
+      types = @config[:writers].select {|w| w["format"] == format }
+      if types.none?
+        raise "No media type defined for format: #{format}"  
+      end
+      types.first["type"]
+    end
+    
+    def format
+      params.key?("format") ? params["format"][0] : "json"
     end
 
     def client
@@ -48,10 +72,6 @@ module Rack
 
     def filter?
       params.keys.select{|param| param[/filter-.+|not-.+/]}.any?
-    end
-
-    def csv?
-      params.has_key?('format') && params['format'] == ["csv"]
     end
 
   end
