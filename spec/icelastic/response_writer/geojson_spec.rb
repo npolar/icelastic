642d853e512d201db3efb079044545f016c938f4
cnrdh <cnrdh@users.noreply.github.com>
2017-08-30 14:29:35 +0200

    Data as column arrays in properties; \nConfigurable coordinate keys; \nNow nullable: geometry,properties; \nSupport type = Feature

--- spec/icelastic/response_writer/geojson_spec.rb
+++ spec/icelastic/response_writer/geojson_spec.rb
@@ -5,30 +5,26 @@ describe Icelastic::ResponseWriter::GeoJSON do
   def response
     {
       "feed" => {
-        "stats" => [
+        "opensearch" => {},
+        "facets" => [
           {
-            "doc_count" => 140,
-            "longitude" => {
-                "count" => 140,
-                "min" => -2.629000000000019,
-                "max" => 22.905,
-                "avg" => 18,
-                "sum" => 2566.348,
-                "sum_of_squares" => 49462.788684,
-                "variance" => 17.277977482448996,
-                "std_deviation" => 4.156678659993937
-            },
-            "latitude" => {
-              "avg" => 69,
-            },
-            "positioned" => "1991-01-31T12:00:00Z",
-            "filter" => "4w-positioned"
-          }
-        ],
+          "year-positioned": [
+          {
+          "term": "2015",
+          "count": 9,
+          "uri": ""}
+          ]
+          }],
         "entries" => [{
           "latitude" => 69,
           "longitude" => 18,
-          "title" => "test"
+          "prop1" => "val1",
+          "sea_surface_temperature" => -1,
+        },{
+          "lat" => -90,
+          "lng" => -180,
+          "alt" => -1000,
+          "sea_surface_temperature" => 0,
         }]
       }
     }
@@ -48,47 +44,127 @@ describe Icelastic::ResponseWriter::GeoJSON do
     Icelastic::ResponseWriter::GeoJSON.new(request, r)
   end
 
-  context "normal" do
+  context "format=geojson" do
 
-    it "not return a feed header" do
-      g = geojson(mock_request("q=&format=geojson")).build
-      g.should_not have_key("feed")
+    let!(:fc) { geojson(mock_request("q=&format=geojson")).build }
+    let!(:features) { fc["features"] }
+    let!(:feature) { features[0] }
+
+    it "should return a GeoJSON FeatureCollection" do
+      expect(fc.keys).to eq(["type", "features"])
+      fc["type"].should eq("FeatureCollection")
     end
 
-    it "type = FeatureCollection" do
-      g = geojson(mock_request("q=&format=geojson")).build
-      g["type"].should eq("FeatureCollection")
+    it "where all features have a geometry and type" do
+      features.size.should eq(response["feed"]["entries"].size)
+      expect(features.all? {|f| f.key? "geometry"}).to be(true)
+      expect(features.all? {|f| f.key? "geometry"}).to be(true)
     end
 
-    it "with Point geometry [default]" do
-      g = geojson(mock_request("q=&format=geojson")).build
-      expect(g["features"].map {|f|f["geometry"]}.all? {|g| g["type"] == "Point"}).to be(true)
-    end
-    
-    it "with Point geometry when geometry=point" do      
-      g = geojson(mock_request("q=&format=geojson&geometry=point")).build
-      expect(g["features"].map {|f|f["geometry"]}.all? {|g| g["type"] == "Point"}).to be(true)
+    it "use longitude, latitude as default keys for coordinates" do
+      expect(feature["geometry"]).to eq({"type"=>"Point", "coordinates"=>[18, 69]})
     end
 
-    it "with LineString geometry when &geometry=linestring" do
-      g = geojson(mock_request("q=&format=geojson&geometry=linestring")).build
-      expect(g["features"].map {|f|f["geometry"]}.all? {|g| g["type"] == "LineString"}).to be(true)
+    context "geometry" do
+
+      it "Point featured is set when requested via geometry=Point or when geometry is blank" do
+        expect(features.map {|f|f["geometry"]}.all? {|g| g["type"] == "Point"}).to be(true) # blank
+        g = geojson(mock_request("q=&format=geojson&geometry=Point")).build
+        expect(g["features"].map {|f|f["geometry"]}.all? {|g| g["type"] == "Point"}).to be(true)
+      end
+
+      it "LineString features are returned when geometry=LineString" do
+        g = geojson(mock_request("q=&format=geojson&geometry=LineString")).build
+        expect(g["features"].map {|f|f["geometry"]}.all? {|g| g["type"] == "LineString"}).to be(true)
+      end
+
+      it "MultiPoint features are returned when &geometry=MultiPoint" do
+        g = geojson(mock_request("q=&format=geojson&geometry=MultiPoint")).build
+        expect(g["features"].map {|f|f["geometry"]}.all? {|g| g["type"] == "MultiPoint"}).to be(true)
+      end
+
+      it "should set geometry to null when &geometry=null" do
+        g = geojson(mock_request("q=&format=geojson&geometry=null")).build
+        expect(g["features"].map {|f| f["geometry"]}.all? {|g| g.nil? }).to be(true)
+      end
     end
 
-    it "return a MultiPoint feature when &geometry=multipoint" do
-      g = geojson(mock_request("q=&format=geojson&geometry=MultiPoint")).build
-      expect(g["features"].map {|f|f["geometry"]}.all? {|g| g["type"] == "MultiPoint"}).to be(true)
+    it "return facets when requested" do
+      g = geojson(mock_request("q=&format=geojson&facets=positioned")).build
+      g.should have_key("facets")
+    end
+
+    context "feature" do
+      it { expect(feature).not_to have_key("longitude") }
+      it { expect(feature).not_to have_key("latitude") }
+      it { expect(feature).not_to have_key("altitude") }
+      context "properties" do
+        it { expect(feature["properties"]).not_to have_key("longitude") }
+        it { expect(feature["properties"]).not_to have_key("latitude") }
+        it { expect(feature["properties"]).not_to have_key("altitude") }
+        it { expect(feature["properties"]).to eq( {"prop1"=>"val1", "sea_surface_temperature"=>-1} )}
+      end
+
+    end
+
+    it "override keys with coordinates=lng,lat,alt" do
+      response = { "feed" => { "entries" => [{"lat" => -90,"lng" => -180, "alt" => -1000}] } }
+      g = geojson(mock_request("q=&format=geojson&coordinates=lng,lat,alt"), response).build
+      g["features"][0].should eq( {"type"=>"Feature", "geometry"=>{"type"=>"Point", "coordinates"=>[-180, -90, -1000]}, "properties"=>nil})
+    end
+end
+  context "variant=atom" do
+    it "response should include feed object with collection links and opensearch object" do
+      g = geojson(mock_request("q=&format=geojson&variant=atom")).build
+      g.should have_key("feed")
+      g["feed"].should have_key("opensearch")
+    end
+    # it "response without &variant=atom should not include a feed object" do
+    #   it { expect(fc).to_not have_key("feed") }
+    # end
+  end
+
+  context "LineString" do
+    let!(:feed) {
+      JSON.parse('{ "feed": { "entries": [{
+"sea_surface_temperature": -7.9,
+"buoy": "SNOW_2015b",
+"measured": "2015-04-23T18:00:00Z",
+"longitude": 2.9088047574879434,
+"latitude": 79.45492190977492
+},
+{
+"buoy": "SNOW_2015b",
+"measured": "2015-04-23T19:00:00Z",
+"longitude": 2.9331071457802356,
+"latitude": 79.46223555222599
+},
+{
+"sea_surface_temperature": -6.8,
+"buoy": "SNOW_2015b",
+"measured": "2015-04-23T20:00:00Z",
+"longitude": 2.927528373433181,
+"latitude": 79.47636862975541
+}]}')
+    }
+    let!(:ls) { geojson(mock_request("q=&format=geojson&geometry=LineString&variables=sea_surface_temperature,measured&filter-buoy=SNOW_2015b"), feed).build }
+    let!(:geometry) { ls["features"][0]["geometry"]}
+
+    describe "geometry" do
+      it("type = LineString") { expect(geometry["type"]).to eq("LineString") }
+      describe "coordinates" do
+        it { expect(geometry["coordinates"]).to eq([[2.9088047574879434, 79.45492190977492],[2.9331071457802356, 79.46223555222599],[2.927528373433181, 79.47636862975541]]) }
+      end
+    end
+
+    describe "properties" do
+      it { expect(ls["features"][0]["properties"]).to eq( {"buoy"=>"SNOW_2015b", "measured"=>["2015-04-23T18:00:00Z", "2015-04-23T19:00:00Z", "2015-04-23T20:00:00Z"], "sea_surface_temperature"=>[-7.9, nil, -6.8]}) }
     end
 
   end
 
-  context "stats" do
-
-    it "should use the avg position" do
-      g = geojson(mock_request("q=&format=geojson")).build.to_json
-      g.should include('"coordinates":[18,69]')
-    end
-
-  end
+  # context "stats" do
+  #   #@todo
+  # end
 
 end
